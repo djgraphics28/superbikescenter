079394215b4d583396605b0ff81c8035094aff56
djgraphics28 <darwin.ibay30@gmail.com>
2024-06-27 10:09:43 +0800

    updates

--- app/Livewire/Home.php
+++ app/Livewire/Home.php
@@ -7,6 +7,7 @@ use App\Models\Brand;
 use App\Models\Product;
 use Livewire\Component;
 use App\Models\Category;
+use Livewire\Attributes\Url;
 use Livewire\WithPagination;
 use Spatie\SchemaOrg\Schema;
 
@@ -15,16 +16,17 @@ class Home extends Component
     use WithPagination;
 
     public $selectedPriceRange;
+    #[Url]
     public $selectedBrand;
+    #[Url]
     public $selectedCategory;
 
-
+    #[Url]
     public $minPrice = 0;
+    #[Url]
     public $maxPrice = 300000;
 
-    protected $listeners = ['priceRangeUpdated'];
-
-    protected $queryString = ['minPrice', 'maxPrice', 'selectedBrand', 'selectedCategory'];
+    // protected $queryString = ['minPrice', 'maxPrice', 'selectedBrand', 'selectedCategory'];
 
     /**
      * Render the component.
@@ -53,13 +55,6 @@ class Home extends Component
         $categories = Category::all();
 
         return view('livewire.home', ['products' => $this->products, 'brands' => $brands, 'categories' => $categories]);
-    }
-
-    public function priceRangeUpdated($data)
-    {
-        $this->minPrice = $data['minPrice'];
-        $this->maxPrice = $data['maxPrice'];
-        $this->resetPage(); // If using pagination, reset to page 1
     }
 
     /**
