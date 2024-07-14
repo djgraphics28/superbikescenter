56971defc1a3f152e633f49c6906f0e32aa9e16a
darwin ibay <darwin.i@shoredigitalinc.com>
2024-07-14 21:22:03 +0800

    update

--- app/Http/Controllers/API/InquiryController.php
+++ app/Http/Controllers/API/InquiryController.php
@@ -32,8 +32,8 @@ class InquiryController extends Controller
             'email' => 'required|email|max:255',
             'message' => 'required|string',
             'contact_number' => 'nullable|string|max:15',
-            'province_id' => 'nullable|integer|exists:provinces,id',
-            'city_id' => 'nullable|integer|exists:cities,id',
+            'province_id' => 'nullable|string|exists:provinces,id',
+            'city_id' => 'nullable|string|exists:cities,id',
             'barangay' => 'nullable|string|max:255',
         ]);
 
