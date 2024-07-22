ca5a970fcd7dcca63cc719cb7d5a9cd112dc5074
djgraphics28 <darwin.ibay30@gmail.com>
2024-07-22 21:34:31 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,10 @@ use App\Livewire\Home;
 use App\Livewire\Post\Show as PostShow;
 use App\Livewire\Product\Show as ProductShow;
 use App\Livewire\Motorcycle\Index as MotorcyclShow;
+use App\Livewire\About\Index as AboutPage;
+use App\Livewire\Contact\Index as ContactPage;
+use App\Livewire\Auth\Login as Login;
+use App\Livewire\Auth\Register as Register;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class)->name('home');
@@ -12,5 +16,18 @@ Route::get('/article/{post:slug}', PostShow::class)->name('post.show');
 Route::get('/product/{product:slug}', ProductShow::class)->name('product.show');
 
 Route::get('/motorcycles', MotorcyclShow::class)->name('motorcycles.index');
+Route::get('/about', AboutPage::class)->name('about.index');
+Route::get('/contact', ContactPage::class)->name('contact.index');
 
 Route::get('/customer/register', CustomerRegister::class)->name('customer.register');
+
+
+Route::get('login', Login::class)->name('login');
+Route::get('register', Register::class)->name('register');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/logout', function() {
+        auth()->logout();
+        return redirect()->route('home');
+    })->name('logout');
+});
